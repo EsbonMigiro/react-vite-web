205ac654ae441e69a6f023bcab3bd60d0c6b2671
EsbonMigiro <esbonmigiro@gmail.com>
2024-11-04 22:20:07 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+
+function App() {
+  return (
+    <div className="m-12">
+      {/* <section className="h-screen bg-twitter-blue flex justify-center items-center m-12 md:p-12 lg:p-14 xl:p-20">
+        <p>coding with react on vite being an awsome project to do</p>
+      </section> */}
+      <div className="card rounded">This is a card</div>
+    </div>
+  );
+}
+
+export default App;
